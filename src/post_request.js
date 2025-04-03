90eb252c37a4b9911532c144080db2918b2755b6
lucky <me@toaster.com>
2025-04-03 21:11:47 +0200

    another commit

--- src/post_request.js
+++ src/post_request.js
@@ -6,27 +6,30 @@ import showdown from 'showdown'
 const { Converter } = showdown
 
 
-const ds = new DeepSeek(Models.REASON, process.env.DEEPSEEK_KEY)
+const deepseek = new DeepSeek(Models.REASON, process.env.DEEPSEEK_KEY)
 const mdToHtml = new Converter()
 
 
 export default async function processPostRequest(path, mimetype, req) {
     let cv = null
     try {
+
         if (mimes['docx'] === mimetype) cv = await Textract.docx(path)
         else if (mimes['pdf'] === mimetype) cv = await Textract.pdf(path)
-        console.log(`Extracting ${req.file.originalname} text => Done`)
 
-        const cvClean = removePersonalInfo(cv)
-        const summary = await ds.completion(summaryPrompt(cvClean)).then(r => r.choices[0].message.content).then(r => removeMarkdown(r))
-        const advice = await ds.completion(advicePrompt(summary)).then(r => r.choices[0].message.content)
+        const summary = await deepseek.completion(summaryPrompt(removePersonalInfo(cv)))
+            .then(r => r.choices[0].message.content)
+            .then(r => removeMarkdown(r))
+
+        const advice = await deepseek.completion(advicePrompt(summary))
+            .then(r => r.choices[0].message.content)
+
         const response =  mdToHtml.makeHtml(summary + '\n\n---\n\n' + advice)
-        console.log('Generating LLM response => Done')
-
         await Mail.send(req.body.email, response)
-        console.log('Sending email => Done')
+        console.log(`[${new Date().toLocaleString()}] ${req.body.email} => Done`)
+
     } catch (e) {
-        console.error(e.message)
+        console.error(`${req.body.email} => ${e.message}`)
     } finally {
         await unlink(join(__dirname, path)).catch(e => console.error(e.message))
     }
